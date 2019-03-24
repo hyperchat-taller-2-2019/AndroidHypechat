ecccae1bba13f70df7dafd4c8ca9fd714008355b
DiegoForgan <dmforgan89@gmail.com>
2019-03-23 23:53:20 -0300

    Diseño de pantalla Login
    Agrego diseño de pantalla login + logo de slack. Todavia puede mejorarse.

--- app/src/main/java/com/example/hypechat/MainActivity.java
+++ app/src/main/java/com/example/hypechat/MainActivity.java
@@ -1,42 +1,43 @@
 package com.example.hypechat;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText textoEmail;
+    private EditText textoPassword;
+
 
     //Este es un ejemplo de una request simple GET hecha con el singleton de la clase HttpConexionSingleton
-    public void makeRequest (View view){
-        Toast.makeText(this, "por hacer request!", Toast.LENGTH_SHORT).show();
-        String url = "https://siu-api.herokuapp.com/";
-        StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Toast.makeText(MainActivity.this, response, Toast.LENGTH_LONG).show();
-            }
-        },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Algo salio mal con la Request", Toast.LENGTH_LONG).show();
-                    }
-                });
-        HttpConexionSingleton.getInstance(this).addToRequestQueue(request);
+    public void login (View view){
+        String email = this.textoEmail.getText().toString();
+        String password = this.textoPassword.getText().toString();
+
+        Toast.makeText(this, "Email: " + email + "\n" + "Password: " + password, Toast.LENGTH_LONG).show();
+
+    }
+
+    public void loginConFacebook (View view){
+        Toast.makeText(this, "Aca te deberias loguear con Facebook!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void registro (View view){
+        Toast.makeText(this, "Aca te deberias Registrar!", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textoEmail = (EditText) findViewById(R.id.et_email);
+        this.textoPassword = (EditText) findViewById(R.id.et_password);
     }
 
 }
