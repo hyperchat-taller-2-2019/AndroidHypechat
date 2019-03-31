17c8006bcaa20d2597cce8dd9912319ae00763b7
DiegoForgan <dmforgan89@gmail.com>
2019-03-31 17:07:22 -0300

    Le pego al Mock de Swagger
    Le pego al mock de swagger para hacer el login y agrego validaciones para saber si el usuario es valido.
    Mejoro la UI cuando loguea.

--- app/src/main/java/com/example/hypechat/MainActivity.java
+++ app/src/main/java/com/example/hypechat/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hypechat;
 
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -27,55 +28,77 @@ public class MainActivity extends AppCompatActivity {
     private ValidadorDeCampos validador;
     private SharedPreferences sharedPref;
     private SharedPreferences.Editor sharedEditor;
+    private ProgressDialog progressDialog;
 
     //ESTA DESPUES DEBERIA SER LA DIRECCION DE DONDE ESTE EL SERVER REAL Y EL ENDPOINT CORRESPONDIENTE!
-    private final String url = "https://hypechat-taller2-2019.herokuapp.com/autenticacion/login";
+    private final String url = "https://virtserver.swaggerhub.com/taller2-hypechat/Hypechat/1.0.0/login";
 
     public void login (View view){
+        Log.i("INFO", "Intentando realizar el login a la app");
+
         String email = this.textoEmail.getText().toString();
         String password = this.textoPassword.getText().toString();
         this.textoEmail.setText("");
         this.textoPassword.setText("");
 
-
-
         if (this.validador.areValidLoginFields(email,password,this)){
-            String URL_completa = this.url + "?email=" + email + "&contrasenia=" + password;
+
+            this.progressDialog = ProgressDialog.show(this,"Hypechat","Validando datos...",
+                    true);
+
+            String URL_completa = this.url + "?email=" + email + "&contraseña=" + password;
             // SE PUEDE HACER EL REQUEST AL SERVER PARA LOGUEARSE !
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.GET, URL_completa, null, new Response.Listener<JSONObject>() {
 
                         @Override
                         public void onResponse(JSONObject response) {
-                            try{
-                                String apodo_response = response.getString("apodo");
-                                String nombre_response = response.getString("nombre");
-                                String email_response = response.getString("email");
-                                sharedEditor.putString("apodo",apodo_response);
-                                sharedEditor.putString("nombre",nombre_response);
-                                sharedEditor.putString("email",email_response);
-                                sharedEditor.apply();
-                                goHomeActivity();
-                            }
-                            catch (JSONException error){
-                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();;
-                            }
-
+                            progressDialog.dismiss();
+                            procesarResponse(response);
                         }
 
                     }, new Response.ErrorListener() {
 
                         @Override
                         public void onErrorResponse(VolleyError error) {
-                            Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            Toast.makeText(MainActivity.this,
+                                    "No fue posible conectarse al servidor, por favor intente de nuevo mas tarde", Toast.LENGTH_LONG).show();
 
                         }
                     });
+
             //Agrego la request a la cola para que se conecte con el server!
             HttpConexionSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
         }
 
 
+    }
+
+    private void procesarResponse(JSONObject response) {
+        try{
+            //Usuario valido?
+            if (response.getInt("valido") == 0){
+                String token_response = response.getString("token");
+                String apodo_response = response.getString("apodo");
+                String nombre_response = response.getString("nombre");
+                String email_response = response.getString("email");
+                sharedEditor.putString("apodo",apodo_response);
+                sharedEditor.putString("nombre",nombre_response);
+                sharedEditor.putString("email",email_response);
+                sharedEditor.putString("token",token_response);
+                sharedEditor.apply();
+                goHomeActivity();
+            }
+            else{
+                Toast.makeText(MainActivity.this,
+                        "Usuario o Contraseña Invalidos!", Toast.LENGTH_LONG).show();
+            }
+        }
+        catch (JSONException error){
+            Toast.makeText(MainActivity.this,
+                    "Hubo un error con el Json de Respuesta", Toast.LENGTH_SHORT).show();;
+        }
     }
 
     private void goHomeActivity() {
@@ -97,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("INFO","Se esta iniciando la aplicación");
 
         //SharedPref para almacenar datos de sesión
         this.sharedPref = getSharedPreferences(getString(R.string.saved_data), Context.MODE_PRIVATE);
@@ -107,7 +131,7 @@ public class MainActivity extends AppCompatActivity {
         this.sharedEditor.clear();
         this.sharedEditor.apply();
 
-
+        Log.i("INFO", "Tomando referencias de la UI");
         //Referencias del layout
         this.validador = new ValidadorDeCampos();
         this.textoEmail = (EditText) findViewById(R.id.et_email);
