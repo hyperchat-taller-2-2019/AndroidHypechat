6a006ae7359d68a9b51a98f8f77e1638616609da
DiegoForgan <dmforgan89@gmail.com>
2019-04-21 02:52:22 -0300

    Header de la navigation Bar muestra nombre del usuario.

--- app/src/main/java/com/example/hypechat/HomeActivity.java
+++ app/src/main/java/com/example/hypechat/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.example.hypechat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -9,18 +11,25 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
 
     private DrawerLayout drawer;
+    private View header;
+    private SharedPreferences sharedPref;
+    private SharedPreferences.Editor sharedEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        //SharedPref para almacenar datos de sesión
+        this.sharedPref = getSharedPreferences(getString(R.string.saved_data), Context.MODE_PRIVATE);
+        this.sharedEditor = sharedPref.edit();
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -31,6 +40,11 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = findViewById(R.id.navView);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Setea el header de la navigation bar para que tenga el nombre de usuario (podria ser apodo o mail)
+        this.header = navigationView.getHeaderView(0);
+        TextView header_nombre_usuario = (TextView) this.header.findViewById(R.id.header_user_name);
+        header_nombre_usuario.setText(this.sharedPref.getString("nombre","No Name"));
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
